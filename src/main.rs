c06e17f99ee65a5fd6259118a4317bb91f12557b
rcMarty <korotwitschkamartin@gmail.com>
2025-01-14 23:06:30 +0100

    git2 playground something

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use git2::Repository;
+use git2::{Commit, Error, Oid, Repository};
 use octocrab::{models, params, Octocrab};
 use octocrab::auth::Auth::UserAccessToken;
+use octocrab::models::issues::Issue;
 use secrecy::SecretString;
 use serde::Serialize;
 
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
-    octocrab_playground().await.expect("octocrab_playground failed");
+    //octocrab_playground().await.expect("octocrab_playground failed");
+    git2_playground().expect("git2_playground failed");
 }
 
 #[derive(serde::Deserialize, Debug)]
@@ -26,11 +28,73 @@ fn load_config() -> SecretString {
     SecretString::new(Box::from(config.token.secret))
 }
 
-fn git2_playground() {
-    let repo = match Repository::open("/path/to/a/repo") {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to open: {}", e),
-    };
+fn git2_playground() -> Result<(), Error> {
+    let repo = Repository::open("test_repos/team")?;
+
+    for status in repo.statuses(Some(git2::StatusOptions::new().include_untracked(true)))?.iter() {
+        println!("status: {:?}", status.status());
+    }
+
+    //get all commit ids
+    let mut revwalk = repo.revwalk()?;
+    revwalk.push_head()?;
+    revwalk.set_sorting(git2::Sort::TOPOLOGICAL | git2::Sort::TIME)?;
+
+    for id in revwalk.take(10) {
+        let id = id?;
+        let commit = repo.find_commit(id)?;
+        println!("commit: {:?}", commit.message().unwrap());
+    }
+
+    //get modified files
+
+    //94cfefb8a926c6f040369b9779ee55248534684d
+    //cf72bbeccb270686f411d7bffa2cb24339dd9592
+    let oid = Oid::from_str("94cfefb8a926c6f040369b9779ee55248534684d")?;
+    let res = repo.find_commit(oid)?;
+    println!("commit: {:?}", res.message().unwrap());
+
+    // get information about the commit
+    let commit = repo.find_commit(oid)?;
+    let diff_tree = commit.tree()?;
+    println!("diff tree {:?} with tree id {:?}", diff_tree, commit.tree_id());
+    for diff in diff_tree.iter() {
+        println!("tree entry name: {:?}", diff.name().unwrap());
+        println!("tree entry kind {:?}", diff.kind().unwrap());
+    }
+
+    // changed files
+    let parent = commit.parent(0)?;
+    let diff = repo.diff_tree_to_tree(Some(&parent.tree()?), Some(&commit.tree()?), None)?;
+    for delta in diff.deltas() {
+        println!("delta: {:?}", delta.new_file().path().unwrap());
+    }
+
+    //show canged lines in files
+    let diff = repo.diff_tree_to_tree(Some(&parent.tree()?), Some(&commit.tree()?), None)?;
+    for delta in diff.deltas() {
+        let old_file = delta.old_file();
+        let new_file = delta.new_file();
+        let old_oid = old_file.id();
+        let new_oid = new_file.id();
+        let old_blob = repo.find_blob(old_oid)?;
+        let new_blob = repo.find_blob(new_oid)?;
+        let old_content = String::from_utf8_lossy(old_blob.content());
+        let new_content = String::from_utf8_lossy(new_blob.content());
+        let old_lines = old_content.lines();
+        let new_lines = new_content.lines();
+
+        println!("old content: {:?}", old_content);
+        println!("new content: {:?}", new_content);
+
+        for (i, (old_line, new_line)) in old_lines.zip(new_lines).enumerate() {
+            if old_line != new_line {
+                println!("line {} changed from {} to {}", i + 1, old_line, new_line);
+            }
+        }
+    }
+
+    Ok(())
 }
 
 async fn octocrab_playground() -> octocrab::Result<()> {
@@ -38,7 +102,8 @@ async fn octocrab_playground() -> octocrab::Result<()> {
     println!("secret: {:?}", load_config());
 
     let octocrab = Octocrab::builder().personal_token(login).build()?;
-    let octocrab = octocrab::initialise(octocrab);
+    let _ = octocrab::initialise(octocrab); // for whatever reason this doesnt return octocrab with auth
+    let octocrab = octocrab::instance(); //this is needed to return right octocrab with auth
 
 
     let page = octocrab.issues("rust-lang", "team")
@@ -51,14 +116,20 @@ async fn octocrab_playground() -> octocrab::Result<()> {
 
     println!("Found {} issues, no of pages: {:?}, total count: {:?}", page.items.len(), page.number_of_pages(), page.total_count);
 
-    let results = octocrab.all_pages::<models::issues::Issue>(page).await?;
+    // let results = octocrab.all_pages::<models::issues::Issue>(page).await?;
 
-    println!("there is overall {} issues", results.len());
+    let result = octocrab.get_page::<Issue>(&page.next).await?;
+    println!("result: {:?}", result.clone().unwrap().items.first().unwrap().url);
+    let issue = result.unwrap().items;
+    // println!("there is overall {} issues", results.len());
 
-    for issue in results.iter().take(5) {
+    // for issue in results.iter().take(5) {
+    //     println!("#{}: {} {}\nauthor(s): {:?}\nlabels: {:?}\n", issue.number, issue.title, issue.body_text.clone().unwrap_or("No body".to_string()), issue.user.login, issue.labels, );
+    // }
+
+    for issue in issue {
         println!("#{}: {} {}\nauthor(s): {:?}\nlabels: {:?}\n", issue.number, issue.title, issue.body_text.clone().unwrap_or("No body".to_string()), issue.user.login, issue.labels, );
     }
-
 
     Ok(())
 }
