60eef59484a0d61df38f46b620b38f0a706be19b
rcMarty <korotwitschkamartin@gmail.com>
2025-08-27 17:42:54 +0200

    at least its starting

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,19 +1,18 @@
-use schemars::JsonSchema;
-
 pub mod webhooks;
 pub mod review;
 pub mod app_state;
 pub mod docs;
 
-#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, JsonSchema)]
+#[derive(serde::Deserialize, Debug, Clone, schemars::JsonSchema)]
 pub struct ReviewParams {
     file: String,
     last_n_days: Option<i64>,
     from_date: Option<chrono::NaiveDateTime>,
 }
 
-#[derive(serde::Deserialize, JsonSchema)]
+#[derive(serde::Deserialize, schemars::JsonSchema)]
 pub struct Pagination {
     skip: Option<i32>,
     page: Option<i32>,
 }
+
